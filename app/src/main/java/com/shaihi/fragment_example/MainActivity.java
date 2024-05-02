dd40bebd6ad537a3eea8d9ccdf65d836cee94e7d
shaihi <shaihi@icloud.com>
2024-05-02 14:09:39 +0300

    Added the code to switch between two fragments

--- app/src/main/java/com/shaihi/fragment_example/MainActivity.java
+++ app/src/main/java/com/shaihi/fragment_example/MainActivity.java
@@ -3,6 +3,7 @@ package com.shaihi.fragment_example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirstFragment firstFragment = new FirstFragment();
+        SecondFragment secondFragment = new SecondFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+
+        Button frg1Btn = findViewById(R.id.fragOneBtn);
+        Button frg2Btn = findViewById(R.id.fragTwoBtn);
+
+        frg1Btn.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
+        });
+        frg2Btn.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
+        });
     }
 }
